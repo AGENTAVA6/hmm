01146f57da7ad4349fe7209b827b558ff573cd9d
AGENTAVA6 <72601590+AGENTAVA6@users.noreply.github.com>
2020-12-12 22:51:42 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,21 @@
+player1_score = 0;
+player2_score = 0;
+player_1 = localStorage.getItem("player1_name");
+player_2 = localStorage.getItem("player2_name");
+document.getElementById("player1_name").innerHTML = player_1;
+document.getElementById("player2_name").innerHTML = player_2;
+document.getElementById("player1_score").innerHTML = ":" + player1_score;
+document.getElementById("player2_score").innerHTML = ":" + player2_score;
+function send(){
+    number1 = document.getElementById("number_1").value;
+    number2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+    question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='umm'>"
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'> Check </button>"
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number_1").value = "";
+    document.getElementById("number_2").value = "";
+}
+
---
+++ login2.js
@@ -0,0 +1,5 @@
+function user(){
+    window.location = "game_page.html";
+    player1_name = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player2_name",player2_name);
+}
